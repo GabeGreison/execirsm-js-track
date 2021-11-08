db0aa889154b940818087d649f35a6185720b082
Gabriel Greison <gabrielgsousa72@gmail.com>
2021-11-08 19:38:40 -0300

    :tada:first commit

---
+++ bird-watcher/bird-watcher.js
@@ -0,0 +1,31 @@
+
+export function totalBirdCount(birdsPerDay) {
+    let sum =0;
+    
+    for (let i = 0; i < birdsPerDay.length; i++){
+      sum += birdsPerDay[i];
+    }
+  
+    return sum
+  }
+  
+  export function birdsInWeek(birdsPerDay, week) {
+    let sum =0;
+    let weekDays = 7;
+    
+    for (let i = (week * weekDays) - weekDays; i < week * weekDays; i++) {
+      sum += birdsPerDay[i]
+    }
+  
+    return sum
+  }
+  
+  export function fixBirdCountLog(birdsPerDay) {
+    
+      for (let i = 0; i < birdsPerDay.length; i+=2){
+      birdsPerDay[i] = birdsPerDay[i] +1;
+    }
+  
+    return birdsPerDay
+  }
+  
